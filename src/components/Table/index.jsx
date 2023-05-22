56e29b6c7bca70a8fb2113129f48cf21cbd3cf50
Marcelo Martins <marcelocmartins98@gmail.com>
2023-05-22 17:28:48 -0300

    fix table

--- src/components/Table/index.jsx
+++ src/components/Table/index.jsx
@@ -1,63 +1,24 @@
-import { useState } from "react";
 import { Container, TableStyled } from "./styles";
 
 export function Table(info) {
-  const [played, setPlayed] = useState();
-  const [wins, setWins] = useState();
-  const [draws, setDraws] = useState();
-  const [loses, setLoses] = useState();
-
-  function calculate(result) {
-    let sum = 0;
-    for (const [, value] of Object.entries(result)) {
-      sum += value;
-    }
-    return sum;
-  }
-
-  function calculatePlayed() {
-    const sum = calculate(info.info.played);
-    setPlayed(sum);
-  }
-  function calculateWins() {
-    const sum = calculate(info.info.wins);
-    setWins(sum);
-  }
-  function calculateDraws() {
-    const sum = calculate(info.info.draws);
-    setDraws(sum);
-  }
-  function calculateLoses() {
-    const sum = calculate(info.info.loses);
-    setLoses(sum);
-  }
-
-  useState(() => {
-    console.log(info);
-    calculatePlayed();
-    calculateWins();
-    calculateDraws();
-    calculateLoses();
-  }, []);
-
   return (
     <Container>
       <TableStyled>
         <thead>
           <tr>
-            <th>Jogos</th>
             <th>Vitórias</th>
             <th>Derrotas</th>
             <th>Empates</th>
+            <th>Jogos</th>
           </tr>
         </thead>
         <tbody>
           {info ? (
             <tr>
-              <td>{played}</td>
-              <td>{wins}</td>
-              <td>{draws}</td>
-              <td>{loses}</td>
+              <td>{info.info.played.total}</td>
+              <td>{info.info.wins.total}</td>
+              <td>{info.info.draws.total}</td>
+              <td>{info.info.loses.total}</td>
             </tr>
           ) : null}
         </tbody>
